18af62c4f58cf6aba876551c122a2da1305b452e
Luke Grazette <Luke.Grazette@CERN.CH>
2022-10-14 10:26:59 +0000

    Add the tutorial c++ code

---
+++ tut.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+int main(){
+    //Create double and integer variables
+    //Output the product of these
+    double a{75.9};
+    int b{11};
+    std::cout << a * b << std::endl;
+
+    //Create another string variable
+    //Create a single char variable, assign it the value of one of the 
+    //letters in the string, and then output this variable
+    std::string msg{"This is a string"};
+    char c{msg[2]};
+
+    std::cout << c << std::endl;
+
+    return 0;
+}
